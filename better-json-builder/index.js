6c8ca883827fc89c6cef80136c46dda73c8b4544
lgdd <louis-guillaume.durand@liferay.com>
2023-06-10 14:02:50 +0200

    feat: add file with latest liferaycloud images

--- better-json-builder/index.js
+++ better-json-builder/index.js
@@ -1,4 +1,5 @@
 const http = require('https');
+const axios = require('axios');
 const fs = require('fs');
 const { execSync } = require('child_process');
 const cliProgress = require('cli-progress');
@@ -8,37 +9,37 @@ const productInfoJsonUrl =
 
 const productJsonFileName = '.product_info.json';
 const productJsonFile = fs.createWriteStream(productJsonFileName);
-http.get(productInfoJsonUrl, function (response) {
-  response.pipe(productJsonFile);
-  productJsonFile.on('finish', () => {
-    productJsonFile.close();
-    console.log(`Downloaded ${productJsonFileName}`);
-    fs.readFile(productJsonFileName, 'utf8', (err, data) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-      const betterJson = buildBetterJson(JSON.parse(data));
-      writePromotedProductsJson(betterJson);
+// http.get(productInfoJsonUrl, function (response) {
+//   response.pipe(productJsonFile);
+//   productJsonFile.on('finish', () => {
+//     productJsonFile.close();
+//     console.log(`Downloaded ${productJsonFileName}`);
+//     fs.readFile(productJsonFileName, 'utf8', (err, data) => {
+//       if (err) {
+//         console.error(err);
+//         return;
+//       }
+//       const betterJson = buildBetterJson(JSON.parse(data));
+//       writePromotedProductsJson(betterJson);
 
-      writeTypedProductsJson(betterJson, 'dxp');
-      writeTypedProductsJson(betterJson, 'dxp', '7.4');
-      writeTypedProductsJson(betterJson, 'dxp', '7.3');
-      writeTypedProductsJson(betterJson, 'dxp', '7.2');
-      writeTypedProductsJson(betterJson, 'dxp', '7.1');
-      writeTypedProductsJson(betterJson, 'dxp', '7.0');
+//       writeTypedProductsJson(betterJson, 'dxp');
+//       writeTypedProductsJson(betterJson, 'dxp', '7.4');
+//       writeTypedProductsJson(betterJson, 'dxp', '7.3');
+//       writeTypedProductsJson(betterJson, 'dxp', '7.2');
+//       writeTypedProductsJson(betterJson, 'dxp', '7.1');
+//       writeTypedProductsJson(betterJson, 'dxp', '7.0');
 
-      writeTypedProductsJson(betterJson, 'portal');
-      writeTypedProductsJson(betterJson, 'portal', '7.4');
-      writeTypedProductsJson(betterJson, 'portal', '7.3');
-      writeTypedProductsJson(betterJson, 'portal', '7.2');
-      writeTypedProductsJson(betterJson, 'portal', '7.1');
-      writeTypedProductsJson(betterJson, 'portal', '7.0');
+//       writeTypedProductsJson(betterJson, 'portal');
+//       writeTypedProductsJson(betterJson, 'portal', '7.4');
+//       writeTypedProductsJson(betterJson, 'portal', '7.3');
+//       writeTypedProductsJson(betterJson, 'portal', '7.2');
+//       writeTypedProductsJson(betterJson, 'portal', '7.1');
+//       writeTypedProductsJson(betterJson, 'portal', '7.0');
 
-      writeTypedProductsJson(betterJson, 'commerce');
-    });
-  });
-});
+//       writeTypedProductsJson(betterJson, 'commerce');
+//     });
+//   });
+// });
 
 const buildBetterJson = (productJson) => {
   const betterJson = [];
@@ -131,3 +132,68 @@ const decodeBundleUrl = (encodedBundleUrl, releaseDate) => {
   );
   return bundleUrlDecodeResultBytes.toString().trim();
 };
+
+// Liferay Cloud Images
+
+const latestStableVersionRegex =
+  /^(\d+\.\d+\.\d+(-jdk\d+)?|^\d+\.\d+(-jdk\d+)?)(-\d+\.\d+\.\d+)?$/;
+const dockerHubRepoApiBaseUrl =
+  'https://registry.hub.docker.com/v2/repositories';
+
+const liferayCloudImages = [
+  'liferaycloud/backup',
+  'liferaycloud/jenkins',
+  'liferaycloud/database',
+  'liferaycloud/liferay-dxp',
+  'liferaycloud/elasticsearch',
+  'liferaycloud/nginx',
+];
+
+const fetchLatestTag = async (liferayCloudImage, page) => {
+  try {
+    const response = await axios.get(
+      `${dockerHubRepoApiBaseUrl}/${liferayCloudImage}/tags?page_size=1024&page=${page}`
+    );
+    let results = response.data.results;
+    latestTag = findLatestTag(liferayCloudImage, results);
+    if (latestTag === '') {
+      return await fetchLatestTag(liferayCloudImage, page + 1);
+    } else {
+      return latestTag;
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const findLatestTag = (liferayCloudImage, results) => {
+  for (let i = 0; i < results.length; i++) {
+    if (latestStableVersionRegex.test(results[i].name)) {
+      results[i][
+        'latest_docker_image'
+      ] = `${liferayCloudImage}:${results[i].name}`;
+      return results[i];
+    }
+  }
+  return '';
+};
+
+const fetchLatestTagPromises = liferayCloudImages.map(
+  async (liferayCloudImage) => {
+    return await fetchLatestTag(liferayCloudImage, 1);
+  }
+);
+
+const writeLatestLiferayCloudImages = async () => {
+  const latestDockerImages = await Promise.all(fetchLatestTagPromises);
+  fs.writeFile(
+    '../liferaycloud_latest_docker_images.json',
+    JSON.stringify(latestDockerImages, null, '\t'),
+    function (err) {
+      if (err) throw err;
+      console.log('Write liferaycloud_latest_docker_images.json');
+    }
+  );
+};
+
+writeLatestLiferayCloudImages();
