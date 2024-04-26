159a1d45b8c133a396a4f15b51f82fa134037261
lgdd <louis-guillaume.durand@liferay.com>
2024-04-26 10:57:42 +0200

    feat: add releases parsing & files building

--- src/main.go
+++ src/main.go
@@ -1,19 +1,26 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
 	"io"
+	"log"
+	"net"
 	"net/http"
 	"os"
+	"strings"
+	"time"
 )
 
 func main() {
 	fetchLatestWorkspaceGradlePlugin()
+	buidReleaseFiles()
 }
 
 func fetchLatestWorkspaceGradlePlugin() {
-	resp, err := http.Get("https://search.maven.org/solrsearch/select?q=a:com.liferay.gradle.plugins.workspace&rows=1&wt=json")
+	httpClient := getHttpClient()
+	resp, err := httpClient.Get("https://search.maven.org/solrsearch/select?q=a:com.liferay.gradle.plugins.workspace&rows=1&wt=json")
 
 	if err != nil {
 		panic(err)
@@ -40,6 +47,185 @@ func fetchLatestWorkspaceGradlePlugin() {
 	}
 }
 
+func buidReleaseFiles() {
+	start := time.Now()
+	httpClient := getHttpClient()
+	fmt.Print("Get releases.json")
+	resp, err := httpClient.Get("https://releases.liferay.com/releases.json")
+
+	if err != nil {
+		fmt.Printf(" ❌ (%.2f s)\n", time.Since(start).Seconds())
+		panic(err)
+	}
+
+	fmt.Printf(" ✅ (%.2f s)\n", time.Since(start).Seconds())
+	start = time.Now()
+	defer resp.Body.Close()
+	body, _ := io.ReadAll(resp.Body)
+
+	var releases []Release
+	json.Unmarshal(body, &releases)
+
+	var dxp74, dxp73, dxp72, dxp71, dxp70 []Release
+	var portal74, portal73, portal72, portal71, portal70 []Release
+
+	for _, release := range releases {
+		release.FetchProperties()
+		if release.Product == "dxp" {
+			dxp74, dxp73, dxp72, dxp71, dxp70 = updateDXPReleases(release, dxp74, dxp73, dxp72, dxp71, dxp70)
+		}
+		if release.Product == "portal" {
+			portal74, portal73, portal72, portal71, portal70 = updatePortalReleases(release, portal74, portal73, portal72, portal71, portal70)
+		}
+	}
+
+	fmt.Printf("\nDXP 7.4 (%v releases)\n", len(dxp74))
+	fmt.Printf("DXP 7.3 (%v releases)\n", len(dxp73))
+	fmt.Printf("DXP 7.2 (%v releases)\n", len(dxp72))
+	fmt.Printf("DXP 7.1 (%v releases)\n", len(dxp71))
+	fmt.Printf("DXP 7.0 (%v releases)\n\n", len(dxp70))
+
+	fmt.Printf("Portal 7.4 (%v releases)\n", len(portal74))
+	fmt.Printf("Portal 7.3 (%v releases)\n", len(portal73))
+	fmt.Printf("Portal 7.2 (%v releases)\n", len(portal72))
+	fmt.Printf("Portal 7.1 (%v releases)\n", len(portal71))
+	fmt.Printf("Portal 7.0 (%v releases)\n", len(portal70))
+
+	var dxpReleases, portalReleases []Release
+
+	dxpReleases = append(dxpReleases, dxp74...)
+	dxpReleases = append(dxpReleases, dxp73...)
+	dxpReleases = append(dxpReleases, dxp72...)
+	dxpReleases = append(dxpReleases, dxp71...)
+	dxpReleases = append(dxpReleases, dxp70...)
+
+	portalReleases = append(portalReleases, portal74...)
+	portalReleases = append(portalReleases, portal73...)
+	portalReleases = append(portalReleases, portal72...)
+	portalReleases = append(portalReleases, portal71...)
+	portalReleases = append(portalReleases, portal70...)
+
+	writeReleaseFile("dxp", "", dxpReleases)
+	writeReleaseFile("portal", "", portalReleases)
+
+	writeReleaseFile("dxp", "7.4", dxp74)
+	writeReleaseFile("dxp", "7.3", dxp73)
+	writeReleaseFile("dxp", "7.2", dxp72)
+	writeReleaseFile("dxp", "7.1", dxp71)
+	writeReleaseFile("dxp", "7.0", dxp70)
+
+	writeReleaseFile("portal", "7.4", portal74)
+	writeReleaseFile("portal", "7.3", portal73)
+	writeReleaseFile("portal", "7.2", portal72)
+	writeReleaseFile("portal", "7.1", portal71)
+	writeReleaseFile("portal", "7.0", portal70)
+}
+
+func writeReleaseFile(edition, version string, releases []Release) {
+	var pathBuilder strings.Builder
+
+	pathBuilder.WriteString("releases/")
+	pathBuilder.WriteString(edition)
+	if version != "" {
+		pathBuilder.WriteString("_")
+		pathBuilder.WriteString(strings.ReplaceAll(version, ".", ""))
+	}
+	pathBuilder.WriteString("_releases.json")
+
+	fileContent, _ := json.MarshalIndent(releases, "", " ")
+
+	err := os.WriteFile(pathBuilder.String(), fileContent, 0644)
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func updateDXPReleases(release Release, dxp74, dxp73, dxp72, dxp71, dxp70 []Release) ([]Release, []Release, []Release, []Release, []Release) {
+	if release.ProductGroupVersion == "7.4" || strings.Contains(release.ProductGroupVersion, ".q") {
+		dxp74 = append(dxp74, release)
+	}
+	if release.ProductGroupVersion == "7.3" {
+		dxp73 = append(dxp73, release)
+	}
+	if release.ProductGroupVersion == "7.2" {
+		dxp72 = append(dxp72, release)
+	}
+	if release.ProductGroupVersion == "7.1" {
+		dxp71 = append(dxp71, release)
+	}
+	if release.ProductGroupVersion == "7.0" {
+		dxp70 = append(dxp70, release)
+	}
+	return dxp74, dxp73, dxp72, dxp71, dxp70
+}
+
+func updatePortalReleases(release Release, portal74, portal73, portal72, portal71, portal70 []Release) ([]Release, []Release, []Release, []Release, []Release) {
+	if release.ProductGroupVersion == "7.4" {
+		portal74 = append(portal74, release)
+	}
+	if release.ProductGroupVersion == "7.3" {
+		portal73 = append(portal73, release)
+	}
+	if release.ProductGroupVersion == "7.2" {
+		portal72 = append(portal72, release)
+	}
+	if release.ProductGroupVersion == "7.1" {
+		portal71 = append(portal71, release)
+	}
+	if release.ProductGroupVersion == "7.0" {
+		portal70 = append(portal70, release)
+	}
+	return portal74, portal73, portal72, portal71, portal70
+}
+
+func (release *Release) FetchProperties() {
+	httpClient := getHttpClient()
+	start := time.Now()
+	fmt.Print("Get " + release.URL + "/release.properties")
+	resp, err := httpClient.Get(release.URL + "/release.properties")
+
+	if err != nil {
+		fmt.Printf(" ❌ (%.2f s)\n", time.Since(start).Seconds())
+		panic(err)
+	}
+
+	fmt.Printf(" ✅ (%.2f s)\n", time.Since(start).Seconds())
+
+	defer resp.Body.Close()
+	config, err := ReadPropertiesFile(resp.Body)
+	release.ReleaseProperties = ReleaseProperties{
+		AppServerTomcatVersion: config["app.server.tomcat.version"],
+		BuildTimestamp:         config["build.timestamp"],
+		BundleChecksumSha512:   config["bundle.checksum.sha512"],
+		BundleURL:              config["bundle.url"],
+		GitHashLiferayDocker:   config["git.hash.liferay-docker"],
+		GitHasLiferayPortalEE:  config["git.hash.liferay-portal-ee"],
+		LiferayDockerImage:     config["liferay.docker.image"],
+		LiferayDockerTags:      config["liferay.docker.tags"],
+		LiferayProductVersion:  config["liferay.product.version"],
+		ReleaseDate:            config["release.date"],
+		TargetPlatformVersion:  config["target.platform.version"],
+	}
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func getHttpClient() http.Client {
+	return http.Client{
+		Transport: &http.Transport{
+			Dial: (&net.Dialer{
+				Timeout:   60 * time.Second,
+				KeepAlive: 30 * time.Second,
+			}).Dial,
+			TLSHandshakeTimeout: 60 * time.Second,
+		},
+		Timeout: 120 * time.Second,
+	}
+}
+
 type MavenCentralSearchResponse struct {
 	Body struct {
 		NumFound int `json:"numFound"`
@@ -56,3 +242,55 @@ type MavenCentralSearchResponse struct {
 		Suggestions []any `json:"suggestions"`
 	} `json:"spellcheck"`
 }
+
+func ReadPropertiesFile(filecontent io.Reader) (AppConfigProperties, error) {
+	config := AppConfigProperties{}
+
+	scanner := bufio.NewScanner(filecontent)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if equal := strings.Index(line, "="); equal >= 0 {
+			if key := strings.TrimSpace(line[:equal]); len(key) > 0 {
+				value := ""
+				if len(line) > equal {
+					value = strings.TrimSpace(line[equal+1:])
+				}
+				config[key] = value
+			}
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+
+	return config, nil
+}
+
+type Release struct {
+	Product               string            `json:"product"`
+	ProductGroupVersion   string            `json:"productGroupVersion"`
+	ProductVersion        string            `json:"productVersion"`
+	Promoted              string            `json:"promoted"`
+	ReleaseKey            string            `json:"releaseKey"`
+	TargetPlatformVersion string            `json:"targetPlatformVersion"`
+	URL                   string            `json:"url"`
+	ReleaseProperties     ReleaseProperties `json:"releaseProperties"`
+}
+
+type ReleaseProperties struct {
+	AppServerTomcatVersion string `json:"appServerTomcatVersion"`
+	BuildTimestamp         string `json:"buildTimestamp"`
+	BundleChecksumSha512   string `json:"bundleChecksumSha512"`
+	BundleURL              string `json:"bundleURL"`
+	GitHashLiferayDocker   string `json:"gitHashLiferayDocker"`
+	GitHasLiferayPortalEE  string `json:"gitHashLiferayPortalEE"`
+	LiferayDockerImage     string `json:"liferayDockerImage"`
+	LiferayDockerTags      string `json:"liferayDockerTags"`
+	LiferayProductVersion  string `json:"liferayProductVersion"`
+	ReleaseDate            string `json:"releaseDate"`
+	TargetPlatformVersion  string `json:"targetPlatformVersion"`
+}
+
+type AppConfigProperties map[string]string
